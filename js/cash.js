dadbe42f4f4e072a1a6cf60870f119873d55dacc
ycy <604028466@qq.com>
2018-12-21 17:01:41 +0800

    web集中实验

--- js/cash.js
+++ js/cash.js
@@ -6,7 +6,29 @@ window.onload = function () {
     var all_choose = document.getElementById('all-choose');
     var checks = document.getElementsByTagName('input');
     var money = document.getElementById('money');
+    var price = document.getElementsByClassName('price');
     var money_hint = document.getElementById('money-hint');
+    var btn_delete = document.getElementsByClassName('book-delete');
+    var chooseBook = document.getElementsByClassName('chooseBook1');
+    var result = 0;
+
+
+    setInterval(function () {
+
+        for (let i = 0; i < btn_delete.length; i++) {
+            btn_delete[i].onclick = function () {
+                chooseBook[i].style.display = 'none';
+                price[i].innerHTML = '0';
+            }
+        }
+
+        for (var i = 0; i < price.length; i++) {
+            result += parseFloat(price[i].innerHTML)
+        }
+        money.innerText = result;
+        result = 0;
+
+    }, 100);
 
 
     //全选checkbox
@@ -24,6 +46,8 @@ window.onload = function () {
                 money_hint.style.display = 'none';
             }
         }
-    }
+    };
 
-};
+
+}
+;
